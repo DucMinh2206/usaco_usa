1d1bfdfa42f5c7c6f9e1dfdd88e9c7ad5858650a
DucMinh2206 <112641650+DucMinh2206@users.noreply.github.com>
2023-06-09 08:25:12 +0700

    Create milk.cpp

---
+++ simulation/milk.cpp
@@ -0,0 +1,28 @@
+//#include "/Library/Developer/CommandLineTools/usr/include/c++/v1/bits/stdc++.h"  
+#include <bits/stdc++.h>
+using namespace std;
+const int maxN = 2e5 + 5;
+#define foru(i, a, b) for(int i = a; i <= b; ++i)
+#define ford(i, a, b) for(int i = a; i >= b; --i)
+#define mp(a, b) make_pair(a, b)
+vector<pair<int, pair<string, int>>> current;
+bool cmp(pair<int, pair<string, int>> a, )
+int cnt[4];
+signed main(){
+   int n;
+   cin >> n;
+
+   foru(i, 1, n){
+      int x, val;
+      string s;
+      char dau;
+
+      cin >> x >> s >> dau >> val;
+      current.push_back(mp(x, mp(s, val)));
+   }
+   sort()
+   for(pair<int, pair<string, int>> num : current){
+
+   }
+
+}
