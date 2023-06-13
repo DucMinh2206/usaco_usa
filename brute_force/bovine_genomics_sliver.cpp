87be062128615c8dfa599a5dda8ef3dd3c3a942e
DucMinh2206 <112641650+DucMinh2206@users.noreply.github.com>
2023-06-13 13:01:01 +0700

    Update bovine_genomics_sliver.cpp

--- brute_force/bovine_genomics_sliver.cpp
+++ brute_force/bovine_genomics_sliver.cpp
@@ -1,4 +1,4 @@
-
+#define MINHDEPTRAI 1
 #ifdef MINHDEPTRAI
 #include "/Library/Developer/CommandLineTools/usr/include/c++/v1/bits/stdc++.h"  
 #include <chrono>
@@ -10,12 +10,12 @@ using namespace std ::chrono;
 using namespace std;
 #define foru(i, a, b) for(int i = a; i <= b; ++i)
 #define ford(i, a, b) for(int i = a; i >= b; --i)
+#define IOS ios_base:: sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define mp(a, b) make_pair(a, b)
-#define endl '\n'
-#define int long long
 
 const int maxN = 500 + 5;
 const int maxM = 50 + 5;
+
 const long long inf = 1e17;
 char normal[maxN][maxM], spoty[maxN][maxM];
 string current_no[maxN], current_spo[maxN];
@@ -25,7 +25,6 @@ void plus_no(string &ss, int a, int b, int c, int p){
     ss += normal[p][a];
     ss += normal[p][b];
     ss += normal[p][c];
-
 }
 
 void plus_spo(string &ss, int a, int b, int c, int p){
@@ -36,9 +35,11 @@ void plus_spo(string &ss, int a, int b, int c, int p){
 }
 
 signed main(){
-    freopen("cownomics.in", "r", stdin);
-    freopen("cownomics.out", "w", stdout);
+    IOS
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
     // input
+
     int n, m;
     cin >> n >> m;
     foru(i, 1, n){
@@ -57,44 +58,40 @@ signed main(){
     #ifdef MINHDEPTRAI
     auto start =   high_resolution_clock::now();
     #endif
+
     // running
     int ans = 0;
     foru(i, 1, m){
         foru(j, i + 1, m){
             foru(k, j + 1, m){
                 bool check = false;
-
+                set<string> curr_fi, curr_se, curr_megre;
                 foru(p, 1, n){
                     plus_no(current_no[p], i, j, k, p);
+                    curr_fi.insert(current_no[p]);
                 }
 
                 foru(p, 1, n){
                     plus_spo(current_spo[p], i, j, k, p);
+                    curr_se.insert(current_spo[p]);
                 }
+                // merge time
+                curr_megre = curr_fi;
+                for(string s: curr_se) curr_megre.insert(s);
 
-                for(int i = 1; i <= n && !check; i++){
-                    for(int j = 1; j <= n && !check; j++){
-                        if(current_no[i] == current_spo[j]){
-                            check = true;
-                            break;
-                        }
-                    }
-                }
-
-                if(!check){
+                if(curr_megre.size() == curr_fi.size() + curr_se.size()){
                     ans++;
                 }
+
             }
         }
     }
-
     cout << ans;
-    return 0;
     // end
     #ifdef MINHDEPTRAI
     auto end = high_resolution_clock::now();
     auto exacution = duration_cast<microseconds> (end - start);
-    cout << "  "<< exacution.count() << "ms";
+    cout << "  "<< exacution.count() / 1000 << "ms";
     #endif
-
+    
 }
